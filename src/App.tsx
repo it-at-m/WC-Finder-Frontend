2d1c544a84baa067fc3471468f13f1cb161ec8b7
Payel Banerjee <paayelbanerjee@gmail.com>
2021-11-05 14:45:24 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import Search from "./components/Search/Search";
+import Preview from "./components/Preview/Preview";
+import { connect } from "react-redux";
+import { loadAllPlace } from "./store/actions";
+import { Place } from "./store/models";
+
+const fetchData = async () => {
+  const res = await fetch('http://localhost:5000/')
+  const json = await res.json()
+  return json
+}
+
+function App({loadAllPlaces}: any) {
+  fetchData().then(data => {
+    loadAllPlaces(data);
+  })
+  return (
+    <>
+      <nav>
+        <Header />
+      </nav>
+      <main>
+        <Search />
+        <Map />
+        <Preview />
+      </main>
+    </>
+  );
+}
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    loadAllPlaces: (payload: Place[]) =>
+      dispatch(loadAllPlace(payload))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,43 @@
+import { connect } from "react-redux";
+import { setSearchVisibility } from "../../store/actions";
+import { IState } from "../../store/models";
+import "./Header.css";
+import { CgSearch} from "react-icons/cg";
+
+const Header = ({ searchIsVisible, setSearchVisibility }: any) => {
+
+  return (
+    <div className="header__container">
+       <CgSearch
+        style={{
+          fontSize: "3rem",
+          verticalAlign: "middle",
+          position: "absolute",
+          left: "1rem",
+          top: "10px",
+        }}
+        onClick={() => setSearchVisibility(!searchIsVisible)}
+      ></CgSearch>
+      <span>
+        Loofinder
+        <span role="img" aria-label="toilet">
+          🚽
+        </span>
+      </span>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: IState) => {
+  const { search } = state;
+  return search;
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    setSearchVisibility: (payload: boolean) =>
+      dispatch(setSearchVisibility(payload)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
