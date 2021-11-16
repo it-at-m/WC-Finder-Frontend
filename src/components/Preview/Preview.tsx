c0ce8b11d970610729326d30e127582484667935
Payel Banerjee <paayelbanerjee@gmail.com>
2021-11-16 21:30:10 +0100

    add details in preview

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -5,6 +5,7 @@ import { AiFillCloseCircle } from "react-icons/ai";
 import "./Preview.css";
 import { apiUri } from "../../constants";
 import { ImKey } from "react-icons/im";
+import{ CgArrowsShrinkH } from "react-icons/cg"
 
 
 const Preview = ({ isVisible, place, closePreview }: any) => {
@@ -25,11 +26,14 @@ const Preview = ({ isVisible, place, closePreview }: any) => {
         <div className="preview__title">{place?.title}</div>
         <div className="preview__description">{place?.short_description}</div>
         <div className="grid_container">
-          <div><ImKey className="icon"></ImKey></div>
-          <div className="preview__eurokey">Eurokey: {place?.eurokey === 0 ? "No" : "Yes"}</div>
+          <div><ImKey className="key_icon"></ImKey></div>
+          <div className="preview__eurokey">Eurokey: {place?.eurokey === 0 ? "Not required" : "Required"}</div>
         </div>
-        <div className="preview__ramp_steepness">Ramp Steepness: {place?.ramp_steepness}</div>
-        <div className="preview__door_width">Door Width: {place?.door_width}</div>
+        <div className="preview__ramp_steepness">Ramp Steepness: {place?.ramp_steepness}%</div>
+        <div className="grid_container">
+          <div><CgArrowsShrinkH className="door_icon"></CgArrowsShrinkH></div>
+          <div className="preview__door_width">Door Width: {place?.door_width}cm</div>
+        </div>
         {/* <div style={{display: 'flex'}}>
           <a className="preview__button" href={place?.seeMoreLink} target="_blank" rel="noreferrer">See more</a>
         </div> */}
