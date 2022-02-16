077043ba9f87750e9339c00fd7e327c214932032
yashpupneja <pupneja.yashu@gmail.com>
2022-02-16 13:44:32 +0530

    Info Button Correcction

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   const {t} = useTranslation();
   return (
     <div className="header__container">
-      <button className="header" onClick={()=>window.open("https://forms.gle/YNagaK7RyNroiqfn9")}>
+      <button className="header" onClick={()=>window.open("https://forms.gle/D3AbbSA4GGQ6TMzu9")}>
         {t("Beta Testing Survey")} - inclus
       </button>
     </div>
